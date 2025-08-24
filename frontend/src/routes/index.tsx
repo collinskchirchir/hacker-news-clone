8c9e55ca34548e00ac44301e1e29e1d108ee5e77
Collins Chirchir <collinskchirchir@gmail.com>
2025-08-24 18:47:40 +0300

    eslint setup

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,17 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import * as React from 'react';
+import { createFileRoute } from '@tanstack/react-router';
+
+import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/')({
   component: HomeComponent,
-})
+});
 
 function HomeComponent() {
   return (
-    <div className="p-2">
+    <div className='p-2'>
       <h3>Welcome Home!</h3>
+      <Button>Hellow World</Button>
     </div>
-  )
+  );
 }
